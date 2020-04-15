fdacc4426e81d27f437627f5a292544bcdc9b0da
Andrey <andrey.kappix.haines@gmail.com>
2020-04-15 18:10:03 +0300

    added cors

--- MCBLabber/Startup.cs
+++ MCBLabber/Startup.cs
@@ -1,3 +1,5 @@
+using MCBLabber.JWT;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -18,27 +20,43 @@ namespace MCBLabber
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
             services.AddControllers();
             services.AddDbContext<LabberDBContext>(options => options.UseMySql(Configuration.GetConnectionString("LabberDBContext")));
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = true;
+                    options.TokenValidationParameters = new JWTlabberValidationParameters(new AuthOptions());
+                });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
+            
             app.UseHttpsRedirection();
-            app.UseRouting();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
 
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseCors();
+            app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
+
             using (var scope = app.ApplicationServices.CreateScope())
             {
                 var dbContext = scope.ServiceProvider.GetService<LabberDBContext>();
